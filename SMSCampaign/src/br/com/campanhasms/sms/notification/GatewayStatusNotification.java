8a7d47d9415fcbe1abd5160ee5eb11094d0c6f51
Diegod <Diegod@NOTEOSA1855.infocorp.net>
2012-07-24 13:13:02 -0300

    Force the SMS Service to re-initialize when the Status become STOPPED;

--- SMSCampaign/src/br/com/campanhasms/sms/notification/GatewayStatusNotification.java
+++ SMSCampaign/src/br/com/campanhasms/sms/notification/GatewayStatusNotification.java
@@ -5,6 +5,9 @@ import org.smslib.AGateway;
 import org.smslib.AGateway.GatewayStatuses;
 import org.smslib.IGatewayStatusNotification;
 
+import br.com.campanhasms.persistence.SystemPrevayler;
+import br.com.campanhasms.sms.service.impl.SMSServiceWrapper;
+
 public class GatewayStatusNotification implements IGatewayStatusNotification {
 
 	private static final Logger LOGGER = Logger.getLogger(GatewayStatusNotification.class);
@@ -13,6 +16,14 @@ public class GatewayStatusNotification implements IGatewayStatusNotification {
 	public void process(AGateway gateway, GatewayStatuses oldStatus, GatewayStatuses newStatus) {
 		LOGGER.info("Processing a Gateway Status Notification : OldStatus = " + oldStatus + " => NewStatus = "
 				+ newStatus);
+		if(GatewayStatuses.STOPPED.equals(newStatus)) {
+			LOGGER.info("Force SMS Service to re-initialize...");
+			try {
+				SMSServiceWrapper.initialize(SystemPrevayler.getSystemPrevaylerModel().getCOMPort());
+			} catch (Exception e) {
+				LOGGER.error("Error when re-initializing SMS Service...", e);
+			}
+		}
 	}
 
 }
